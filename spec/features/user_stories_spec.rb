ec26918c5e80a206e0f02cd6e1f560f66ea586da
emilio <emsmnndz@gmail.com>
2016-04-23 17:48:18 +0100

    User stories 3 Planes cannot land in a full airport

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,7 +1,22 @@
 describe 'user stories' do
-  it 'instruct a plane to land' do
-    airport = Airport.new
+  it 'instructs a plane to land' do
+    airport = Airport.new(20)
     plane = Plane.new
     expect{ airport.land(plane) }.not_to raise_error
   end
+
+  it 'instructs planes to take off' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    expect { airport.take_off(plane) }.not_to raise_error
+  end
+
+  it 'prevents plane from landing when the airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect {airport.land(plane)}.to raise_error "Cannot land plane: airport full"
+    end
 end
